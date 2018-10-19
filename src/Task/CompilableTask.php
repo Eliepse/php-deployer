f0011ee0cb34843b00322457566624f006a1b5cc
Eliepse <contact@eliepse.fr>
2018-10-20 01:07:28 +0200

    fix: fix false positive when compiled command empty

--- src/Task/CompilableTask.php
+++ src/Task/CompilableTask.php
@@ -17,6 +17,11 @@ class CompilableTask extends Task implements ShouldCompile
      */
     protected $command_raw;
 
+    /**
+     * @var bool
+     */
+    protected $compiled = false;
+
 
     public function __construct(string $name, string $command_raw)
     {
@@ -32,8 +37,8 @@ class CompilableTask extends Task implements ShouldCompile
      */
     public function run(): void
     {
-        if (empty($this->command))
-            throw new CompileException("Trying to run an uncompiled task.");
+        if (!$this->compiled)
+            throw new CompileException("Trying to run an uncompiled task ({$this->name}).");
 
         parent::run();
     }
@@ -48,5 +53,6 @@ class CompilableTask extends Task implements ShouldCompile
     public function setCompiled(string $compiled): void
     {
         $this->command = $compiled;
+        $this->compiled = true;
     }
 }
